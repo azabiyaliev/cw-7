5ae5b86f2f9dad876e779c19a3d06744afad166d
azabiyaliev <aza.biyaliev@gmail.com>
2024-09-28 17:18:54 +0600

    Создал в директории "components" два компонента ChooseProduct.tsx и ChooseProduct.css. В первом компоненте вывел отрисовку выбранных продуктов, во втором добавил на них стилей. Работаю над выводом по 1-му выбранному продукту.

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import friesImage from './assets/fries.png';
 import coffeeImage from './assets/coffee.png';
 import teaImage from './assets/tea.png';
 import colaImage from './assets/cola.png';
+import {useState} from 'react';
+import ChooseProduct from './components/ChooseProduct.tsx';
+
+type IProducts = {
+  title: string;
+  price: number;
+  image: string;
+}
+
+type IProductsCount = {
+  name: string;
+  count: number;
+  price: number;
+}
 
 const App = () => {
-
-
-  type IProducts = {
-    title: string;
-    price: number;
-    image: string;
-  }
 
   const products: IProducts[] = [
     {title: 'Hamburger', price: 80, image: burgerImage},
@@ -26,16 +33,61 @@ const App = () => {
     {title: 'Cola', price: 40, image: colaImage},
   ];
 
-  const showProducts = products.map((product) => {
-    return (
-        <Products key={product.title} title={product.title} price={product.price} image={product.image} />
-    );
+  const [count, setCount] = useState<IProductsCount[]>([
+    {name: 'Hamburger', count: 0, price: 0},
+    {name: 'Cheeseburger', count: 0, price: 0},
+    {name: 'Fries', count: 0, price: 0},
+    {name: 'Coffee', count: 0, price: 0},
+    {name: 'Tea', count: 0, price: 0},
+    {name: 'Cola', count: 0, price: 0},
+  ]);
+
+  const addProductButton = ((productsName: string, productPrice: number,) => {
+    console.log('btn clicked');
+    setCount(prevState => prevState.map(item => {
+      return item.name === productsName ? {
+        ...item,
+        count: (item.count) + 1,
+        price: (item.price + productPrice),
+      } : item;
+    }));
+    console.log(count);
   });
+
+  const showProducts = products.map((product) => {
+      return (
+          <Products key={product.title} title={product.title} price={product.price} image={product.image} addProduct={() => addProductButton(product.title, product.price)}/>
+      );
+  });
+
+  const deleteProductButton = ((productsName: string, productPrice: number) => {
+    console.log('btn clicked');
+    setCount(prevState => prevState.map(item => {
+      return item.name === productsName ? {
+        ...item,
+        count: (item.count) - item.count,
+        price: (item.price - productPrice),
+      } : item;
+      console.log(item.price);
+    }));
+    console.log(count);
+  });
+
+  const showOrders = count.map(order => {
+      return (
+          <ChooseProduct key={order.name} title={order.name} price={order.price} count={order.count} deleteProduct={() => deleteProductButton(order.name, order.price)}/>
+      );
+  });
+
 
   return (
       <div className='mainDiv'>
         <div className='leftDiv'>
           <h2>Other Details:</h2>
+          <div>
+            {showOrders}
+          </div>
+          <p><strong></strong>{}</p>
         </div>
 
         <div className='rightDiv'>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,11 +6,12 @@ interface ProductsProps {
     title: string;
     price: number | string;
     image: string;
+    addProduct: React.MouseEventHandler;
 }
 
-const Products: React.FC<ProductsProps> = ({title, price, image}) => {
+const Products: React.FC<ProductsProps> = ({title, price, image, addProduct}) => {
     return (
-            <button type='button' className='products'>
+            <button type='button' className='products' onClick={addProduct}>
                 <div>
                     <img src={image} width={70} height={60} alt={title} />
                 </div>
